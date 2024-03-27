80abe123c8556b7ba32f24b7ffae9ec94f493585
unknown <berukberhanua@gmail.com>
2024-03-27 09:31:40 +0300

    deleteMovies completed

--- src/go-movie-crud/main.go
+++ src/go-movie-crud/main.go
@@ -28,6 +28,16 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 }
 
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1])
+		}
+	}
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
 
 }
 
